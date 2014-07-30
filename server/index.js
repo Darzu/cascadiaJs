8310e5fabc3d9755ffb2e3f53a1e98ab376b1d70
darzu <daryl.zuniga@gmail.com>
2014-07-30 10:46:57 -0700

    add notes endpoint

--- server/index.js
+++ server/index.js
@@ -9,12 +9,13 @@ app.use("/app", express["static"]("app"));
 app.use("/test", express["static"]("test"));
 
 // REST backend.
-app.get("/hello/:id", function (req, res) {
+app.get("/notes/:id", function (req, res) {
   // **NOTE**: This should be replaced with an **actual** backend datastore
   // retrieval. For starters, we just pass through the id and give dummy data.
   res.json({
     id: req.params.id,
-    message: "From the server!"
+    title: "title # " + req.params.id,
+    text: "text # " + req.params.id
   });
 });
 
