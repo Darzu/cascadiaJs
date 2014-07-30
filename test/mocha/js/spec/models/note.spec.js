eba8d677559f28f320acc192f087509719b3ad15
darzu <daryl.zuniga@gmail.com>
2014-07-30 16:00:44 -0700

    Added starter specs

---
+++ test/mocha/js/spec/models/note.spec.js
@@ -0,0 +1,25 @@
+define(["backbone", "app/models/note"], function(Backbone, NoteModel) {
+
+  describe("app/models/note", function() {
+
+    beforeEach(function() {
+      this.model = new NoteModel();
+    });
+    afterEach(function() {
+      this.model = null;
+    });
+
+    it("is a bb model", function() {
+      expect(this.model)
+        .to.be.an.instanceof(Backbone.Model);
+    });
+
+    it("should have expected defaults", function() {
+      expect(this.model.get("title")).to.equal("");
+      expect(this.model.get("text")).to.contain("Add Note");
+    });
+    it("can set the title property");
+    it("can set the text property");
+  });
+
+});
